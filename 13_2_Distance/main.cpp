ca80a17d5980338495f3245e038f19177a54ed93
jerry0613 <lionking4234@gmail.com>
2020-06-08 16:40:28 +0800

    test

---
+++ 13_2_Distance/main.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+
+#include "bbcar.h"
+
+
+Ticker servo_ticker;
+Serial pc(USBTX, USBRX);
+Ticker encoder_ticker;
+
+PwmOut pin8(D8), pin9(D9);
+
+DigitalIn pin3(D3);
+
+
+BBCar car(pin8, pin9, servo_ticker);
+
+
+int main() {
+
+    parallax_encoder encoder0(pin3, encoder_ticker);
+
+    encoder0.reset();
+
+
+    car.goStraight(100);
+
+    while(encoder0.get_cm()<30) {
+        pc.printf("%f\r\n", encoder0.get_cm());
+        wait_ms(50);
+    }
+
+    car.stop();
+
+}
